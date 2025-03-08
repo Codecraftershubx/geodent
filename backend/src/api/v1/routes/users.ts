4af09b844f3e8d44cb57976090b036b937747f74
William Inyam <sw.inyam@outlook.com>
2025-03-08 11:38:21 +0000

    :bug:fix: resolve path errors in users router

--- backend/src/api/v1/routes/users.ts
+++ backend/src/api/v1/routes/users.ts
@@ -3,10 +3,11 @@ import controllers from "../controllers/index.js";
 
 const router: Router = express.Router();
 
+router.delete("/:id", controllers.users.delete);
 router.post("/", controllers.users.create);
 router.get("/", controllers.users.read);
-router.post("/:id/delete", controllers.users.delete);
-router.put("/:id/update", controllers.users.update);
+router.get("/:id", controllers.users.read);
+router.put("/:id", controllers.users.update);
 
 router.use("/*", (_: Request, res: Response): void => {
   res.status(404).json({ error: "This users resource doesn't exist" });
