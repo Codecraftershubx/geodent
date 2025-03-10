05b55ff14021fe4d6cc750f3f8f2d6728a3ac8c6
William Inyam <sw.inyam@outlook.com>
2025-03-10 12:26:37 +0000

    :hatching_chick:build: add frontend base code plus login page

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import LoginPage from "./pages/Login";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/SignupForm.tsx
@@ -0,0 +1,131 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Control, FieldPath, useForm } from "react-hook-form";
+import { Button } from "./ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+
+// form schema
+const formSchema = z.object({
+  firstName: z.string().min(2).max(50),
+  lastName: z.string().min(2).max(50),
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+const SignUpForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+        <div className="flex flex-col md:flex-row gap-3 items-start">
+          <SignUpFormField
+            name="firstName"
+            label="First Name"
+            placeholder="James"
+            formControl={form.control}
+            className="flex-1 w-full md:basis-1/2"
+          />
+          <SignUpFormField
+            name="lastName"
+            label="Last Name"
+            placeholder="Jackson"
+            formControl={form.control}
+            className="flex-1 w-full md:basis-1/2"
+          />
+        </div>
+        <div className="flex flex-col md:flex-row gap-3 items-start">
+          <SignUpFormField
+            name="email"
+            label="Email"
+            placeholder="enter your email here"
+            inputType="email"
+            formControl={form.control}
+            className="flex-1 w-full md:basis-1/2"
+          />
+          <SignUpFormField
+            name="password"
+            label="Password"
+            placeholder="your password"
+            inputType="password"
+            formControl={form.control}
+            className="flex-1 w-full md:basis-1/2"
+          />
+        </div>
+        <Button
+          type="submit"
+          className="bg-red-600 shadow-none cursor-pointer hover:bg-red-700"
+        >
+          {" "}
+          Sign up{" "}
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+type TSignUpFormField = {
+  name: FieldPath<z.infer<typeof formSchema>>;
+  label: string;
+  placeholder: string;
+  description?: string;
+  inputType?: string;
+  className?: string;
+  formControl: Control<z.infer<typeof formSchema, any>>;
+};
+
+const SignUpFormField: React.FC<TSignUpFormField> = ({
+  name,
+  label,
+  placeholder,
+  description,
+  inputType,
+  formControl,
+  className,
+}) => {
+  return (
+    <FormField
+      control={formControl}
+      name={name}
+      render={({ field }) => (
+        <FormItem className={className || ""}>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              type={inputType || "text"}
+              {...field}
+            />
+          </FormControl>
+          {description && (
+            <FormDescription className="text-xs">{description}</FormDescription>
+          )}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default SignUpForm;
