28886e1ab323d89e519bfc313db7220510589071
William Inyam <sw.inyam@outlook.com>
2025-03-11 18:05:09 +0000

    :star2:feat: finish countries read controller

--- backend/src/api/v1/controllers/countries/read.ts
+++ backend/src/api/v1/controllers/countries/read.ts
@@ -1,17 +1,35 @@
 import { Request, Response } from "express";
-import type { TPayload, TUserModel } from "../../../../utils/types.js";
 import db from "../../../../db/utils/index.js";
 import utils from "../../../../utils/index.js";
 
 const read = async (req: Request, res: Response): Promise<void> => {
+  // get one country by id
   const { id } = req.params;
   if (id) {
-    console.log("getting country:", id);
-  } else {
-    console.log("getting all countries");
+    const country = await db.client.client.country.findMany({ where: { id } });
+    if (country.length) {
+      return utils.handlers.success(res, {
+        message: "query successful",
+        data: country,
+      });
+    }
+    return utils.handlers.error(res, "general", {
+      message: `country ${id} not found`,
+      status: 404,
+    });
   }
-  res.send();
-  return;
+  // get all countries
+  const countries = await db.client.client.country.findMany();
+  if (countries.length) {
+    return utils.handlers.success(res, {
+      message: "query success",
+      data: countries,
+    });
+  }
+  return utils.handlers.error(res, "general", {
+    message: "no country created yet",
+    status: 404,
+  });
 };
 
 export default read;
