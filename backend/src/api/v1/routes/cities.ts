3388bc6cb9d85367f63fc003eb91a64313ea698a
William Inyam <sw.inyam@outlook.com>
2025-03-11 23:00:59 +0000

    :bug:update: fix cities route error

---
+++ backend/src/api/v1/routes/cities.ts
@@ -0,0 +1,30 @@
+import express, { Response, Request, Router } from "express";
+import { body } from "express-validator";
+import controllers from "../controllers/index.js";
+
+const router: Router = express.Router();
+
+router.get("/", controllers.cities.get);
+router.get("/:id", controllers.cities.get);
+router.post(
+  "/",
+  [
+    body("data")
+      .notEmpty()
+      .withMessage("data is required")
+      .isObject()
+      .withMessage("expects an object"),
+    body(["data.name", "data.stateId"])
+      .notEmpty()
+      .withMessage("required field"),
+    body("data.aka").optional().notEmpty().withMessage("value cannot be empty"),
+  ],
+  controllers.cities.create,
+);
+
+router.use("/*", (_: Request, res: Response): void => {
+  res.status(404).json({ error: "This city resource doesn't exist" });
+  return;
+});
+
+export default router;
