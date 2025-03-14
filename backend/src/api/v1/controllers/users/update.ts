0697bde111bbb8f90263390a050e7d1180813225
William Inyam <sw.inyam@outlook.com>
2025-03-14 13:22:15 +0000

    :up:implement users update route and controller

--- backend/src/api/v1/controllers/users/update.ts
+++ backend/src/api/v1/controllers/users/update.ts
@@ -1,7 +1,44 @@
-import { Response, Request } from "express";
+import { Request, Response } from "express";
+import { matchedData, validationResult } from "express-validator";
+import db from "../../../../db/utils/index.js";
+import utils from "../../../../utils/index.js";
 
-const update = (_: Request, res: Response) => {
-  res.json({ controller: "user Update", message: "success" });
+const update = async (req: Request, res: Response): Promise<void> => {
+  // get one city by id
+  const validation = validationResult(req);
+  if (!validation.isEmpty()) {
+    const validationErrors = validation.array();
+    return utils.handlers.error(res, "validation", {
+      message: "validation error",
+      data: validationErrors,
+      count: validationErrors.length,
+    });
+  }
+
+  const { id } = req.params;
+  const { data } = matchedData(req);
+
+  // verify school exists
+  const user = await db.client.client.user.findMany({
+    where: { id, isDeleted: false },
+  });
+  if (!user.length) {
+    return utils.handlers.error(res, "validation", {
+      status: 404,
+      message: `user not found`,
+    });
+  }
+  // update school
+  let updatedUser = await db.client.client.user.update({
+    where: { id },
+    data,
+  });
+  const filtered = await db.client.filterModels([updatedUser]);
+  return utils.handlers.success(res, {
+    message: "update successful",
+    count: 1,
+    data: filtered,
+  });
 };
 
 export default update;
--- backend/src/api/v1/routes/users.ts
+++ backend/src/api/v1/routes/users.ts
@@ -7,8 +7,32 @@ const router: Router = express.Router();
 router.delete("/:id", controllers.users.delete);
 router.get("/", controllers.users.read);
 router.get("/:id", controllers.users.read);
-router.put("/:id", controllers.users.update);
-
+router.put(
+  "/:id",
+  [
+    body("data")
+      .notEmpty()
+      .withMessage("data is required")
+      .isObject()
+      .withMessage("expects an object"),
+  ],
+  controllers.users.update,
+);
+router.post(
+  "/",
+  [
+    body("data")
+      .notEmpty()
+      .withMessage("data is required")
+      .isObject()
+      .withMessage("expects an object"),
+    body(["data.firstName", "data.lastName", "data.email", "data.password"])
+      .notEmpty()
+      .withMessage("required field"),
+    body("data.role").default("USER"),
+  ],
+  controllers.users.create,
+);
 router.use("/*", (_: Request, res: Response): void => {
   res.status(404).json({ error: "This users resource doesn't exist" });
   return;
