985bfddc3f39604cbf066efc5ec26a4e064cddbe
William Inyam <sw.inyam@outlook.com>
2025-03-21 07:50:30 +0000

    :up:update: add flats missing controllers and expose endpoints

---
+++ backend/src/api/v1/controllers/flats/connections.ts
@@ -0,0 +1,87 @@
+import { Request, Response } from "express";
+import { matchedData, validationResult } from "express-validator";
+import { Prisma } from "@prisma/client";
+import db from "../../../../db/utils/index.js";
+import utils from "../../../../utils/index.js";
+
+const updateConnections = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  const validation = validationResult(req);
+  if (!validation.isEmpty()) {
+    const validationErrors = validation.array();
+    return utils.handlers.error(res, "validation", {
+      message: "validation error",
+      data: validationErrors,
+      count: validationErrors.length,
+    });
+  }
+
+  const { id } = req.params;
+  const { data } = matchedData(req);
+  const connections = ["rooms", "amenities", "documents", "tags"];
+  const connectObject = {} as Prisma.FlatUpdateInput;
+  const { extend } = req.query;
+
+  // verify block exists
+  const flat = await db.client.client.flat.findMany({
+    where: { id, isDeleted: false },
+  });
+  if (!flat.length) {
+    return utils.handlers.error(res, "validation", {
+      status: 404,
+      message: `flat not found`,
+    });
+  }
+  // construct update object
+  for (let field of connections) {
+    // [UPDATE!!] check if each id provided is valid
+    if (data[field]) {
+      if (extend) {
+        Object.assign(connectObject, {
+          [field]: {
+            connect: data[field].map((id: string) => {
+              return { id };
+            }),
+          },
+        });
+      } else {
+        Object.assign(connectObject, {
+          [field]: {
+            disconnect: data[field].map((id: string) => {
+              return { id };
+            }),
+          },
+        });
+      }
+    }
+  }
+  // update flat
+  try {
+    let updated = await db.client.client.flat.update({
+      where: { id },
+      data: { ...connectObject },
+      include: {
+        rooms: { omit: db.client.omit.default },
+        amenities: { omit: db.client.omit.default },
+        documents: { omit: db.client.omit.default },
+        tags: { omit: db.client.omit.default },
+      },
+    });
+    const filtered = await db.client.filterModels([updated]);
+    return utils.handlers.success(res, {
+      message: "update successful",
+      count: 1,
+      data: filtered,
+    });
+  } catch (err: any) {
+    console.log(err);
+    return utils.handlers.error(res, "general", {
+      message: err?.message ?? err.toString(),
+      data: [{ details: err }],
+    });
+  }
+};
+
+export default updateConnections;
--- backend/src/api/v1/controllers/flats/index.ts
+++ backend/src/api/v1/controllers/flats/index.ts
@@ -1,10 +1,12 @@
 import createFlat from "./create.js";
 import deleteFlat from "./delete.js";
+import flatConnections from "./connections.js";
 import readFlats from "./read.js";
 import restoreFlat from "./restore.js";
 import updateFlat from "./update.js";
 
 export default {
+  connections: flatConnections,
   create: createFlat,
   delete: deleteFlat,
   get: readFlats,
