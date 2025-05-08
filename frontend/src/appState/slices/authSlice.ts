929716cc3b4d9f7b23079643edd5335d3e2b6a34
William Inyam <sw.inyam@outlook.com>
2025-05-08 02:47:56 +0000

    :star2::star:feat:handle login and update auth redux state

--- frontend/src/appState/slices/authSlice.ts
+++ frontend/src/appState/slices/authSlice.ts
@@ -1,170 +1,207 @@
 import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
-import type { AppDispatchType, AuthStateType, BEDataType, StoreMessageType, UserType } from "../../utils/types.js";
+import type {
+  AppDispatchType,
+  AuthStateType,
+  BEDataType,
+  StoreMessageType,
+  UserType,
+} from "../../utils/types.js";
 import api from "../../utils/api.js";
 
 type LoginCredentialsType = {
-    accessToken?: string | null;
-    email?: string | null;
-    password?: string | null;
-}
+  accessToken?: string | null;
+  email?: string | null;
+  password?: string | null;
+};
 
-
-// logout user
-const loginUser = createAsyncThunk<UserType, LoginCredentialsType, { rejectValue: BEDataType, dispatch: AppDispatchType }>("auth/login", async (credentials: LoginCredentialsType, { rejectWithValue, dispatch }) => {
+// login user
+const loginUser = createAsyncThunk<
+  UserType,
+  LoginCredentialsType,
+  { rejectValue: BEDataType; dispatch: AppDispatchType }
+>(
+  "auth/login",
+  async (credentials: LoginCredentialsType, { rejectWithValue, dispatch }) => {
     console.log("LOGIN THUNK CALLED WITH:", credentials);
     const options: Record<string, any> = {};
     if (credentials.accessToken) {
-        options.headers = {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${credentials.accessToken}`,
-        }
+      options.headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${credentials.accessToken}`,
+      };
     } else {
-        options.data = {
-            email: credentials.email,
-            password: credentials.password,
-        };
+      options.data = {
+        email: credentials.email,
+        password: credentials.password,
+      };
     }
-    const response = await api.post("/auth/login", options);
+    let response = await api.post("/auth/login", options);
+    let data;
     if (response.error) {
-        return rejectWithValue(response.data);
+      const { message } = response.data.header;
+      if (message === "access token expired") {
+        return { redirect: "/token/refresh" };
+      }
+      return rejectWithValue(response.data);
     }
-    const data = response.data.data[0];
+    data = response.data.data[0];
     if (data.accessToken) {
-        dispatch(authSlice.actions.setAccessToken(data.accessToken));
-        delete data.accessToken;
+      dispatch(authSlice.actions.setAccessToken(data.accessToken));
+      delete data.accessToken;
     }
     return data;
-});
+  },
+);
 
 // logout sequence
-const logoutUser = createAsyncThunk<boolean, Record<string, any>, { rejectValue: BEDataType }>("auth/logout", async ({}, { rejectWithValue }) => {
-    const response = await api.post("/auth/logout", {});
-    if (response.error) {
-        return rejectWithValue(response.data);
-    }
-   return true;
+const logoutUser = createAsyncThunk<
+  boolean,
+  Record<string, any>,
+  { rejectValue: BEDataType }
+>("auth/logout", async ({}, { rejectWithValue }) => {
+  const response = await api.post("/auth/logout", {});
+  if (response.error) {
+    return rejectWithValue(response.data);
+  }
+  return true;
 });
 
 // message toggle
-const toggleMessage = createAsyncThunk<Promise<void>, { autoHide: boolean, delay?: number }, { dispatch: AppDispatchType }>("auth/toggleMessage", async ({ autoHide, delay = 4000 }, { dispatch }) => {
-    dispatch(authSlice.actions.showMessage());
-    if (autoHide) {
-        setTimeout(() => {
-            dispatch(authSlice.actions.hideMessage());
-        }, delay);
-    }
+const toggleMessage = createAsyncThunk<
+  Promise<void>,
+  { autoHide: boolean; delay?: number },
+  { dispatch: AppDispatchType }
+>("auth/toggleMessage", async ({ autoHide, delay = 4000 }, { dispatch }) => {
+  dispatch(authSlice.actions.showMessage());
+  if (autoHide) {
+    setTimeout(() => {
+      dispatch(authSlice.actions.hideMessage());
+    }, delay);
+  }
 });
 const authSlice = createSlice({
-    name: "auth",
-    initialState: {
-        accessToken: window.localStorage.getItem("accessToken") || null,
-        isLoggedIn: false,
-        isLoading: false,
-        showMessage: false,
-        message: null,
-        user: null,
-        error: null,
+  name: "auth",
+  initialState: {
+    accessToken: window.localStorage.getItem("accessToken") || null,
+    isLoggedIn: false,
+    isLoading: false,
+    showMessage: false,
+    message: null,
+    user: null,
+    error: null,
+  },
+  reducers: {
+    setAccessToken: (state: AuthStateType, action: PayloadAction<string>) => {
+      state.accessToken = action.payload;
+      window.localStorage.setItem("accessToken", action.payload);
     },
-    reducers: {
-        setAccessToken: (state: AuthStateType, action: PayloadAction<string>) => {
-            state.accessToken = action.payload;
-            window.localStorage.setItem("accessToken", action.payload);
-        },
-        clearAccessToken: (state: AuthStateType) => {
-            state.accessToken = null;
-            window.localStorage.removeItem("accessToken");
-        },
-        setIsLoggedIn: (state: AuthStateType) => {
-            if (!state.isLoggedIn) {
-                state.isLoggedIn = true;
-            }
-        },
-        unsetIsLoggedIn: (state: AuthStateType) => {
-            if (state.isLoggedIn) {
-                state.isLoggedIn = false;
-            }
-        },
-        setMessage: (state: AuthStateType, action: PayloadAction<StoreMessageType>) => {
-            state.message = action.payload;
-        },
-        clearMessage: (state: AuthStateType) => {
-            state.message = null;
-        },
-        showMessage: (state: AuthStateType) => {
-            state.showMessage = true;
-        },
-        hideMessage: (state: AuthStateType) => {
-            state.showMessage = false;
-        }
+    clearAccessToken: (state: AuthStateType) => {
+      state.accessToken = null;
+      window.localStorage.removeItem("accessToken");
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(loginUser.fulfilled, (state: AuthStateType,  { payload }) => {
-                console.log("LOGIN THUNK FULFILLED REDUCER PAYLOAD:", payload);
-                const data = payload;
-                state.user = data;
-                state.isLoggedIn = true;
-                state.message = {
-                    type: "success", role: "alert", body: "login successful"
-                };
-                state.isLoading = false;
-            })
-        .addCase(loginUser.pending, (state: AuthStateType) => {
-            state.isLoading = true;
-            state.message = {
-                type: "info", role: "alert", body: "logging you in..."
-            };
-        })
-        .addCase(loginUser.rejected, (state: AuthStateType, { payload }) => {
-            console.log("LOGIN THUNK REJECT REDUCER PAYLOAD", payload);
-            const value = payload as BEDataType;
-            state.isLoading = false;
-            state.message = {
-                type: "error", role: "alert", body: `${value.header.message}`,
-                details: value.data,
-                ...value.header
-            };
-            state.isLoggedIn = false;
-        })
-        .addCase(logoutUser.fulfilled, (state: AuthStateType) => {
-            state.isLoading = false;
-            state.isLoggedIn = false;
-            state.message = {
-                type: "success", body: "log out successful", role: "alert"
-            };
-            state.user = null;
-        })
-        .addCase(logoutUser.pending, (state: AuthStateType) => {
-            state.isLoading = true;
-            state.message = {
-                type: "info",
-                role: "alert",
-                body: "logging you out..."
-            };
-        })
-        .addCase(logoutUser.rejected, (state: AuthStateType, { payload }) => {
-            console.log("LOGOUT THUNK REJECTED ERROR:", payload);
-            const value = payload as BEDataType;
-            state.message = {
-                type: "error", role: "notification",
-                body: `${value.header.message}`,
-                details: value.data,
-                ...value.header
-            };
-            state.isLoading = false;
-        })
-    }
+    setIsLoggedIn: (state: AuthStateType) => {
+      if (!state.isLoggedIn) {
+        state.isLoggedIn = true;
+      }
+    },
+    unsetIsLoggedIn: (state: AuthStateType) => {
+      if (state.isLoggedIn) {
+        state.isLoggedIn = false;
+      }
+    },
+    setMessage: (
+      state: AuthStateType,
+      action: PayloadAction<StoreMessageType>,
+    ) => {
+      state.message = action.payload;
+    },
+    clearMessage: (state: AuthStateType) => {
+      state.message = null;
+    },
+    showMessage: (state: AuthStateType) => {
+      state.showMessage = true;
+    },
+    hideMessage: (state: AuthStateType) => {
+      state.showMessage = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.fulfilled, (state: AuthStateType, { payload }) => {
+        console.log("LOGIN THUNK FULFILLED REDUCER PAYLOAD:", payload);
+        const data = payload;
+        state.user = data;
+        state.isLoggedIn = data.redirect ? false : true;
+        state.message = {
+          type: data.redirect ? "info" : "success",
+          role: "alert",
+          body: data.redirect ? "hold on a little longer" : "login successful",
+        };
+        state.isLoading = data.redirect ? true : false;
+      })
+      .addCase(loginUser.pending, (state: AuthStateType) => {
+        state.isLoading = true;
+        state.message = {
+          type: "info",
+          role: "alert",
+          body: "logging you in...",
+        };
+      })
+      .addCase(loginUser.rejected, (state: AuthStateType, { payload }) => {
+        console.log("LOGIN THUNK REJECT REDUCER PAYLOAD", payload);
+        const value = payload as BEDataType;
+        state.isLoading = false;
+        state.message = {
+          type: "error",
+          role: "alert",
+          body: `${value.header.message}`,
+          details: value.data,
+          ...value.header,
+        };
+        state.isLoggedIn = false;
+      })
+      .addCase(logoutUser.fulfilled, (state: AuthStateType) => {
+        state.isLoading = false;
+        state.isLoggedIn = false;
+        state.message = {
+          type: "success",
+          body: "log out successful",
+          role: "alert",
+        };
+        state.user = null;
+      })
+      .addCase(logoutUser.pending, (state: AuthStateType) => {
+        state.isLoading = true;
+        state.message = {
+          type: "info",
+          role: "alert",
+          body: "logging you out...",
+        };
+      })
+      .addCase(logoutUser.rejected, (state: AuthStateType, { payload }) => {
+        console.log("LOGOUT THUNK REJECTED ERROR:", payload);
+        const value = payload as BEDataType;
+        state.message = {
+          type: "error",
+          role: "notification",
+          body: `${value.header.message}`,
+          details: value.data,
+          ...value.header,
+        };
+        state.isLoading = false;
+      });
+  },
 });
 
 export { loginUser, logoutUser, toggleMessage };
 export const {
-    setAccessToken,
-    clearAccessToken,
-    setIsLoggedIn,
-    unsetIsLoggedIn,
-    setMessage,
-    clearMessage,
-    showMessage,
-    hideMessage,
+  setAccessToken,
+  clearAccessToken,
+  setIsLoggedIn,
+  unsetIsLoggedIn,
+  setMessage,
+  clearMessage,
+  showMessage,
+  hideMessage,
 } = authSlice.actions;
 export default authSlice.reducer;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -6,33 +6,44 @@ import { Toaster } from "react-hot-toast";
 import type { RootState } from "../utils/types.js";
 import components from "../components/index";
 
-
-
 const Login: React.FC = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const { accessToken, isLoggedIn } = useAppSelector((store: RootState) => store.auth);
-  const [ heading, setHeading ] = useState( accessToken ? "Welcome Back" : "Login" );
-  const [ runner, setRunner ] = useState(accessToken ? "Hold on while we sign you in" : "Enter your credentials to sign in");
+  const { accessToken, isLoggedIn } = useAppSelector(
+    (store: RootState) => store.auth,
+  );
+  const [heading, setHeading] = useState(
+    accessToken ? "Welcome Back" : "Login",
+  );
+  const [runner, setRunner] = useState(
+    accessToken
+      ? "Hold on while we sign you in"
+      : "Enter your credentials to sign in",
+  );
 
   const login = async (accessToken: string) => {
     try {
       const result = await dispatch(loginUser({ accessToken })).unwrap();
+      console.log("login page result:", result);
+      if (result.redirect) {
+        navigate(result.redirect);
+        return <></>;
+      }
       setHeading(`Welcome ${result.firstName}`);
       setRunner("Taking you in...");
-    } catch(error: any) {
+    } catch (error: any) {
       // attempt token refresh if token expired
       console.error("login page error", error);
       setHeading("Login Again");
       setRunner("Auto login failed. Sign in with your credentials.");
     }
-  }
+  };
 
   useEffect(() => {
     if (isLoggedIn) {
       navigate("/listings");
     }
-  }, [ isLoggedIn ]);
+  }, [isLoggedIn]);
 
   useEffect(() => {
     if (accessToken && !isLoggedIn) {
@@ -44,15 +55,11 @@ const Login: React.FC = () => {
     <main className="flex flex-col justify-between items-center min-h-screen py-10 px-2 md:p-24 ">
       <section className="w-9/10 sm:max-w-md max-w-lg">
         <div className="mb-8 flex flex-col gap-2">
-          <h1 className="text-3xl font-bold text-red-500 mb-5">
-            { heading }
-          </h1>
-          <p className="text-sm text-zinc-500">
-            { runner }
-          </p>
+          <h1 className="text-3xl font-bold text-red-500 mb-5">{heading}</h1>
+          <p className="text-sm text-zinc-500">{runner}</p>
         </div>
         <Toaster />
-        { accessToken === null && <components.LoginForm /> }
+        {accessToken === null && <components.LoginForm />}
       </section>
     </main>
   );
