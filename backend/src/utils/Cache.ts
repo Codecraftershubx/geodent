f5bdc9e161c8781f82a0bfb993dc2da9af97757d
William Inyam <sw.inyam@outlook.com>
2025-06-30 20:20:27 +0000

    :star: create cache class

--- backend/src/utils/Cache.ts
+++ backend/src/utils/Cache.ts
@@ -7,21 +7,63 @@ const cachePort = process.env.CACHE_PORT
 const maxRetries = 10;
 
 class Cache {
+  #name = "CacheAgent";
   #maxRetries = 10;
   #retryInterval = 5000;
   #alive = false;
   #client: RedisClientType | null = null;
+  [Symbol.toStringTag] = this.#name;
 
-  getClient() {
+  // create cache client
+  async init() {
     if (this.#client) {
-      return this.#client;
+      return;
     }
-    this.#client = createClient({
+    this.#client = await createClient({
       socket: {
         host: "localhost",
         port: cachePort,
+        reconnectStrategy: (retries: number) => {
+          if (retries > this.#maxRetries) {
+            return new Error(`${this.#name} failed to initialise`);
+          }
+          const baseDelay = Math.min(2000 ** retries, this.#retryInterval);
+          const jitter = Math.random() * baseDelay;
+          return baseDelay / 2 + jitter / 2;
+        },
       },
-    });
+    })
+      .on("ready", () => {
+        console.log(`[${this.#name}]: Ready ✅`);
+      })
+      .on("connect", () => {
+        console.log(`[${this.#name}]: Trying to connect`);
+      })
+      .on("reconnecting", () => {
+        console.log(`[${this.#name}]: Connection failed. Retrying...`);
+      })
+      .on("error", (err: any) => {
+        if (err.code !== "ECONNREFUSED") {
+          console.error(
+            `${[this.#name]}: ${err?.message ?? "An error occured"}`
+          );
+        }
+      });
+  }
+
+  // connect client
+  async connect() {
+    if (this.#alive || !this.#client) {
+      return;
+    }
+    try {
+      await this.#client.connect();
+      this.#alive = true;
+    } catch (err: any) {
+      console.error(
+        `[${this.#name}]: ${err?.message ?? "Cache connection failed"}`
+      );
+    }
   }
 }
 
