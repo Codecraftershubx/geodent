cd27abba31dcd00130b9a4a93894f45051bd6235
William Inyam <sw.inyam@outlook.com>
2025-07-01 15:39:07 +0000

    :up: update: add cache operation handlers

--- backend/src/utils/Cache.ts
+++ backend/src/utils/Cache.ts
@@ -7,13 +7,17 @@ const cachePort = process.env.CACHE_PORT
 const maxRetries = 10;
 
 class Cache {
-  #name = "CacheAgent";
-  #maxRetries = 10;
-  #retryInterval = 5000;
-  #alive = false;
+  #name: string = "CacheAgent";
+  #maxRetries: number = 10;
+  #retryInterval: number = 5000;
+  #alive: boolean = false;
   #client: RedisClientType | null = null;
+  #defaultExpiry: number = 24 * 7 * 3600;
   [Symbol.toStringTag] = this.#name;
 
+  /*----------------*
+   * CACHE SETUP
+   *----------------*/
   // create cache client
   async init() {
     if (this.#client) {
@@ -52,18 +56,6 @@ class Cache {
           );
         }
       });
-
-    // Safe operation wraper
-    async safeOp (operation: Callable) {
-      if (!this.#alive) {
-        return new Error("Error! Cache not ready");
-      }
-      try {
-        return await operation();
-      } catch (err: any) {
-        return new Error(...err);
-      }
-    }
   }
 
   // connect client
@@ -78,6 +70,84 @@ class Cache {
       console.error(
         `[${this.#name}]: ${err?.message ?? "Cache connection failed"}`
       );
+    }
+  }
+
+  /*----------------------------*
+   * CACHE OPERATION HANDLERS
+   *----------------------------*/
+  // Safe operation wraper
+  async safeOperation(
+    operation: () => Promise<Buffer | string | boolean | null>
+  ) {
+    if (!this.#alive) {
+      throw new Error("Error! Cache not ready");
+    }
+    try {
+      return await operation();
+    } catch (err: any) {
+      throw new Error(...err);
+    }
+  }
+
+  // get string value from cache
+  async get(
+    key: string,
+    buffers: boolean = false,
+    format: BufferEncoding = "binary"
+  ): Promise<Buffer | string | boolean | null> {
+    try {
+      const data = await this.safeOperation(async () => {
+        if (!this.#client) {
+          throw new Error("Error! Cache not ready");
+        }
+        const result = await this.#client.get(key);
+        if (buffers && result) {
+          return Buffer.from(result, format);
+        }
+        return result;
+      });
+      return data;
+    } catch (err: any) {
+      console.error(`[${this.#name}]: ${err?.message}\n\t${err}`);
+      return null;
+    }
+  }
+
+  // save string value to cache
+  async set(
+    key: string,
+    value: string,
+    ex: number = this.#defaultExpiry
+  ): Promise<any | null> {
+    if (!this.#client) {
+      return null;
+    }
+    try {
+      const res = await this.#client.set(key, value, { EX: ex });
+      return res;
+    } catch (err: any) {
+      console.error(`[${this.#name}]: ${err?.message}\n\t${err}`);
+      return null;
+    }
+  }
+
+  // delete a string value(s) from cache
+  async delete(...keys: string[]) {
+    try {
+      const res = await this.safeOperation(async () => {
+        const promises = keys.map((key: string) => {
+          if (!this.#client) {
+            return Promise.reject(new Error("Error! Cache not ready"));
+          }
+          return this.#client.del(key);
+        });
+        await Promise.all(promises);
+        return true;
+      });
+    } catch (err: any) {
+      console.error(`[${this.#name}]: ${err?.message}\n\t${err}`);
+      return false;
     }
   }
 }
