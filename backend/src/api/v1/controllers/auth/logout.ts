bec5f6869643d7849043b634141c3e7005c94515
William Inyam <sw.inyam@outlook.com>
2025-07-03 02:41:09 +0000

    :star::up: update: use in memory cache in logout

--- backend/src/api/v1/controllers/auth/logout.ts
+++ backend/src/api/v1/controllers/auth/logout.ts
@@ -3,29 +3,57 @@ import db from "../../../../db/utils/index.js";
 import utils from "../../../../utils/index.js";
 
 const logout = async (req: Request, res: Response): Promise<void> => {
-  // extract refresh token
-  const { refreshToken } = req.cookies;
-  if (!refreshToken) {
-    return utils.handlers.error(res, "validation", {
-      message: "refresh token required",
-    });
-  }
-
-  // delete token from db
-  try {
-    await db.client.client.user.updateMany({
-      where: { refreshToken },
-      data: { refreshToken: null },
-    });
+  // get auth token from request
+	const authHeader = req.headers.authorization;
+	if (!authHeader) {
+		return utils.handlers.error(res, "authentication", {
+			message: "Unauthorised!",
+		});
+	}
+	const [_, accessToken ] = authHeader.split(" ");
+	if (!accessToken) {
+		return utils.handlers.error(res, "authentication", {
+			message: "Unauthorised!",
+		})
+	}
+	try {
+		// verify user is logged in
+		const cachedUserId = await utils.cache.get(accessToken);
+		if (!cachedUserId) {
+			return utils.handlers.error(res, "authentication", {
+				message: "Unauthorised: user not logged in",
+			})
+		}
+		// extract user id from token and
+		const { payload as aTData } = utils.tokens.decompose.accessToken(accessToken);
+		if (aTData === null) {
+			return utils.errors.error(res, "authentication", {
+				message: "Unauthorised: session expired",
+			});
+		}
+		// validate payload id matches cached user id
+		if (aTData.id !== cachedUserId) {
+			return utils.handlers.error(res, "authentication", {
+				message: "Unauthorised: unknown user",
+			});
+		}
+		// clear tokens from cache
+		const clearSuccess = await utils.cache.delete(accessToken, `${accessToken}${config.refreshCacheSuffix}`);
+		if (!clearSuccess) {
+			return utils.handlers.error(res, "general", {
+				message: "Error: logout failed",
+				code: 500,
+			});
+		}
 
     return utils.handlers.success(res, {
-      message: "logout success",
+      message: "Logout success",
     });
   } catch (err) {
     // error deleting from db
     console.error(err);
     return utils.handlers.error(res, "general", {
-      message: "logout failed",
+      message: "Error: logout failed",
       data: [{ details: err }],
     });
   }
