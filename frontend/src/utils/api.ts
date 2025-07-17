2ebd53642bf06b07277bb2f00e9ac51116221946
William Inyam <sw.inyam@outlook.com>
2025-07-17 00:58:07 +0000

    :up: update: mock expired token refresh

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 
 // create axios instance
 const api = axios.create({
@@ -8,35 +9,98 @@ const api = axios.create({
   maxRedirects: 5,
 });
 
-// default export
-export default {
-  // get requests handler
-  get: async (url: string, options: Record<string, any>) => {
-    try {
-      const res = await api({
-        url,
-        ...options,
-      });
-      return { error: false, content: res.data };
-    } catch (err) {
-      return { error: true, content: err };
-    }
-  },
-  // post requests handler
-  post: async (url: string, options: Record<string, any>) => {
-    try {
-      const res = await api({
-        url,
-        method: "post",
-        headers: { "Content-Type": "application/json", ...options.headers },
-        ...options?.extras,
-      });
-      return { error: false, content: res.data };
-    } catch (err: any) {
-      return {
-        error: true,
-        content: err?.response?.data ?? { header: { message: err.message } },
-      };
-    }
-  },
+/**
+ * API Mocks
+ */
+const mockApi = new MockAdapter(api);
+
+/**
+ * @name Mock.post /auth/refresh
+ */
+mockApi.onPost("/auth/refresh").reply(() => {
+  const expiredRes = [
+    401,
+    {
+      errno: 9,
+      message: "Session expired",
+      status: "failed",
+    },
+  ];
+  const successRes = [
+    200,
+    {
+      accessToken: "h08gehivanur8fhviui948",
+    },
+  ];
+  const responses = [expiredRes, successRes];
+  //const len = responses.length;
+  //const index = Math.floor(Math.random() * 10) % len;
+  return responses[0] as MockResponse;
+});
+
+/**
+ * @name Mock.post /auth/login
+ */
+mockApi.onPost("/auth/login").reply((config) => {
+  const auth = config?.headers?.authorization;
+  console.log(auth);
+  const res = [
+    401,
+    {
+      header: {
+        errno: 5,
+        status: "failed",
+        message: "Expired access token",
+      },
+    },
+  ];
+  return res as MockResponse;
+});
+
+/**
+ * @async @func get Get api request handler
+ * @param {string} url The request url
+ * @param {Record<string, any>} options Extra options to pass to request
+ * @returns {Promise<{error: boolean; content: AxiosResponse | any}>}
+ * @example get(url, { headers: { "X-Name": "X-value"}})
+ */
+const get = async (url: string, options: Record<string, any>) => {
+  try {
+    const res = await api({
+      url,
+      ...options,
+    });
+    return { error: false, content: res.data };
+  } catch (err: any) {
+    return { error: true, content: err };
+  }
 };
+
+/**
+ * @async @func post Post api request handler
+ * @param {string} url The request url
+ * @param {Record<string, any>} options Extra options to pass to request
+ * @returns {Promise<{error: boolean; content: AxiosResponse | any}>}
+ * @example post(url, { data: { "email": "myemail.xyz"}})
+ */
+const post = async (url: string, options: Record<string, any>) => {
+  try {
+    const res = await api({
+      url,
+      method: "post",
+      headers: { "Content-Type": "application/json", ...options.headers },
+      ...options?.extras,
+    });
+    return { error: false, content: res.data };
+  } catch (err: any) {
+    return {
+      error: true,
+      content: err?.response?.data ?? { header: { message: err.message } },
+    };
+  }
+};
+
+type MockResponse = [number, any];
+
+export default { get, post };
+export { api };
