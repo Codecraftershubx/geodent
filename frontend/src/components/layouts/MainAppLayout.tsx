6751ad13bf3e414fe92f6ecb3a25a5416888d17d
William Inyam <sw.inyam@outlook.com>
2025-07-26 23:17:57 +0000

    :pencil: docs: add comment to file + remove debug logs

--- frontend/src/components/layouts/MainAppLayout.tsx
+++ frontend/src/components/layouts/MainAppLayout.tsx
@@ -3,16 +3,22 @@ import { useAppSelector, UseTheme } from "@/hooks/index";
 import type { RootState, MessageType } from "@/lib/types";
 import Components from "../index";
 import ErrorBoundary from "../ErrorBoundary";
+import React from "react";
 
+/**
+ * @func Layout Main App layout
+ * @description Renders navigation at top and sends out children
+ * @returns {React.ReactNode} The Layout With ErrorBoundary
+ */
 const Layout: React.FC = () => {
   const { message, show } = useAppSelector(
     (store: RootState) => store.appMessage
   );
   const { theme } = UseTheme();
-  console.log("theme:", theme);
   return (
-    <div className="bg-gradient" data-theme={theme}>
+    <main className="bg-gradient" data-theme={theme}>
       <Components.NavBar />
+      {/* App alert beneath the navbar */}
       {show && message && (
         <Components.Alert
           type={(message as MessageType).type}
@@ -23,10 +29,11 @@ const Layout: React.FC = () => {
           withTitle={true}
         />
       )}
+      {/* Error boundary over outlet */}
       <ErrorBoundary>
         <Outlet />
       </ErrorBoundary>
-    </div>
+    </main>
   );
 };
 
